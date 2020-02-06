1868d3c0062db11983502755a97a0f797162b427
Evandro Santos <egs.evandro.santos@gmail.com>
2020-02-06 06:51:17 -0300

    Initial Commit

---
+++ exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -0,0 +1,21 @@
+require 'calculator'
+
+describe Calculator do
+    # subject(:calc) { described_class.new }  -> Colocando na variavel calc o valor de subject
+
+    context "#sum" do
+        it "with positive numbers" do
+            result = subject.sum(5, 7)
+            expect(result).to eq(12)
+        end
+    
+        it "with negative numbers" do
+            result = subject.sum(-2, -5)
+            expect(result).to eq(-7)
+        end 
+
+        xit 'teste xit' do
+            # Teste temporariamente sem implementação -> Xit
+        end
+    end
+end
